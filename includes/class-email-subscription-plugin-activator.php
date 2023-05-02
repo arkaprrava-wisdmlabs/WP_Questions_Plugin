225cfbc89a3c680eabb532b9adf3379551047b33
arkaprava.chakraborty@wisdmlabs.com <arkaprava.chakraborty@wisdmlabs.com>
2023-05-02 20:27:15 +0530

    basic boilerplate of the plugin

---
+++ includes/class-email-subscription-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://arkaprava.com
+ * @since      1.0.0
+ *
+ * @package    Email_Subscription_Plugin
+ * @subpackage Email_Subscription_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Email_Subscription_Plugin
+ * @subpackage Email_Subscription_Plugin/includes
+ * @author     Arkaprava <chakraarkaprava@gmail.com>
+ */
+class Email_Subscription_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-email-subscription-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://arkaprava.com
+ * @since      1.0.0
+ *
+ * @package    Email_Subscription_Plugin
+ * @subpackage Email_Subscription_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Email_Subscription_Plugin
+ * @subpackage Email_Subscription_Plugin/includes
+ * @author     Arkaprava <chakraarkaprava@gmail.com>
+ */
+class Email_Subscription_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
